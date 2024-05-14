42d432ac6386c43f5246de912f89ca8325d35e06
leafgilly <themicrowhale@gmail.com>
2024-05-13 23:57:28 -0400

    added glitch text to Amanda

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
-import { Form, Field } from 'react-final-form'
+// import { Form, Field } from 'react-final-form'
 
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DocList from "./pages/DocList";
 import Home from "./pages/Home";
 import Amanda from "./pages/Amanda";
@@ -9,38 +9,13 @@ import Sally from "./pages/Sally";
 import Morgan from "./pages/Morgan";
 import Rita from "./pages/Rita";
 import Page2 from "./pages/Page2";
-
-import React, {useState} from 'react'
 import Styles from './Styles'
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-const onSubmit = async values => {
-  await sleep(300)
-  window.alert(JSON.stringify(values, 0, 2))
-}
-
 function App() {
-  const changeText = () => {
-    window.alert("clicked");
-  //  document.getElementById('vampire').innerHTML = 'newtext';
-  }
-  const [buttonText, setButtonText] = useState('Submit');
- 
-    const handleClick = () => {
-      window.alert('newclick');
-        setButtonText(buttonText === 'Submit' ? 'Loading...' : 'Submit');
-    };
 
   return (
     <Styles>
     <h1>Hello, Brienne.</h1>
-    <div>
-            <h1>Geeksforgeeks</h1>
-            <button onClick={handleClick} >
-                {buttonText}
-            </button>
-        </div>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DocList />}>
--- my-app/src/pages/Amanda.js
+++ my-app/src/pages/Amanda.js
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
-import React, {useState} from 'react'
+import React, {useState, useCallback, useEffect } from 'react'
 
 const Amanda = () => {
 
-    const [buttonText, setButtonText] = useState('Submit');
+    //CODE THAT CHANGES THE DISPLAYED TEXT ON A CLICK
+
+    const [buttonText, setButtonText] = useState('vampire');
  
     const handleClick = () => {
         setButtonText(buttonText === 'vampire' ? 'y̸̘͐ö̸͎ǔ̸͚ ̶̘̈́k̷̼͊i̴͚͝ĺ̴̝l̷͕̂e̷̙͌d̶̞̚ ̵͓̉h̶͚͝e̸͍̎r̴̡̐. YOU BURNED HER WITH THE SUN' : 'vampire');
     };
+
+    //CODE THAT PERIODICALLY CHANGES THE TEXT DISPLAYED ON THE SCREEN USING A TIMER
+
+    const names = [
+        'she is fine', 'she is fine', 'she is fine', "she is d̶͈͒e̶͖͆â̷̙d̸̢̽"
+    ]
+
+    const [newGlitch, setnewGlitch] = useState("");
+
+    const shuffle = useCallback(() => {
+        const index = Math.floor(Math.random() * names.length);
+        console.log(index);
+        setnewGlitch(names[index]);
+    }, []);
+
+    useEffect(() => {
+        const intervalID = setInterval(shuffle, 100);
+        return () => clearInterval(intervalID);
+    }, [shuffle])
 
     return (
     <><h1>Amanda Valentine Documentation</h1>
@@ -19,12 +40,13 @@ const Amanda = () => {
     <h2>Stand and Supernatural Abilities</h2>
     <div>
         <p>something something something <b id="vampire" onClick={handleClick} color="red">{buttonText}</b> something else</p>
+        <p id="glitch" >glitch text</p>
+        <p>{newGlitch}</p>
     </div>
     <h2>Known Relationships</h2>
     <h2>Psychological Profile</h2>
     <h2>Relevant Cases</h2>
     <h2>Author's Notes</h2>
-    <h2></h2>
     <p>
         <Link color="red" to="/">Go Home</Link>
     </p></>
