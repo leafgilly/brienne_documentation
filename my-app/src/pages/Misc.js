e753259c985cc95042f4b333ef118294c2954b5d
leafgilly <themicrowhale@gmail.com>
2024-05-16 21:31:45 -0400

    added css glitch effect + supporters names to Misc

--- my-app/src/pages/Misc.js
+++ my-app/src/pages/Misc.js
@@ -3,11 +3,142 @@ import { Link } from "react-router-dom";
 const Misc = () => {
     return (
     <><h1>Other Active Stand Users</h1>
-    <h2>Stand Users</h2>
+    <div class="wrapper">
+        <h1 class="glitch">Customizable Glitch Text</h1> {/* TO UPDATE THE GLITCH, CHANGE THIS TEXT, THEN GO TO APP.CSS AND MAKE THE CHANGES THERE */}
+    </div>
+    <h2>All Caught Up</h2>
     <ul>
-        <li>Name</li>
-        <li>Name</li>
-        <li>Name</li>
+        <li>Fizzywizard</li>
+        <li>Arc</li>
+        <li>CG the Bird</li>
+        <li>Cream</li>
+        <li>Artemisia</li>
+        <li>Dip</li>
+        <li>Jaded</li>
+        <li>Moe</li>
+        <li>Myles</li>
+        <li>Newbie</li>
+        <li>Daed</li>
+        <li>Gote</li>
+        <li>Poutinerie</li>
+        <li>Rabbit</li>
+        <li>Sah/Diamond</li>
+        <li>Darkdoombob</li>
+        <li>Vamp</li>
+        <li>Cringe</li>
+        <li>Torch</li>
+        <li>Weaponx06</li>
+        <li>Winter</li>
+        <li>Wise</li>
+        <li>Egghead</li>
+        <li>Maxtot</li>
+        <li>Wilbur D Rake</li>
+    </ul>
+    <h2>Friends of the Show/Did Art</h2>
+    <ul>
+        <li>Mal</li>
+        <li>Fetuspie</li>
+        <li>Governor_Explosion</li>
+        <li>Fizz</li>
+        <li>Radon</li>
+        <li>Armes</li>
+        <li>Maeve</li>
+        <li>saintpom</li>
+        <li>ripcarl2k17</li>
+        <li>kal</li>
+        <li>gar</li>
+        <li>smonk</li>
+        <li>cozmic</li>
+        <li>Jellyfly</li>
+        <li>Voice0fReason</li>
+        <li>koalahobbit</li>
+        <li>TheGoodSamaritan</li>
+        <li>bonvoyage_noona</li>
+    </ul>
+    <h2>Read A Bit; Passive Audience</h2>
+    <ul>
+        <li>Jin</li>
+        <li>Ella</li>
+        <li>Shaft</li>
+        <li>bxb777</li>
+        <li>polysinc</li>
+        <li>imaaaaaaaa</li>
+        <li>onion</li>
+        <li>hypernova</li>
+        <li>silvia</li>
+        <li>moose lord</li>
+        <li>carco11</li>
+        <li>GinGerAle</li>
+        <li>BuzzLightyearG</li>
+        <li>osna</li>
+        <li>coop</li>
+        <li>verdlo</li>
+        <li>zerocrosszero</li>
+        <li>omega8</li>
+        <li>normalcardboard</li>
+        <li>SeleneDistress</li>
+        <li>dinoseen</li>
+        <li>banquetBeggar</li>
+        <li>SomethingYouSaid</li>
+        <li>The_Baleful_King</li>
+        <li>Leggoeggo</li>
+        <li>HeroOfTime1435</li>
+        <li>koleoffline</li>
+        <li>medacris</li>
+        <li>brennan</li>
+        <li>ViziDoodle</li>
+        <li>mybedismytruelove</li>
+        <li>pokegirl0238</li>
+        <li>GhostFloRider</li>
+        <li>huffspuffsblows</li>
+        <li>hit_that_target</li>
+        <li>cOnTaGiOuS_ThOuGhTs</li>
+        <li>YeetTheAngels</li>
+    </ul>
+    <h2>Engaged in Some Capacity/Supported Us</h2>
+    <ul>
+        <li>Jigglypuffo</li>
+        <li>Melonbottle</li>
+        <li>carol</li>
+        <li>bonefather</li>
+        <li>bondage fairy</li>
+        <li>winter_bark</li>
+        <li>crowofgayness</li>
+        <li>toasteroven</li>
+        <li>Glass Paper</li>
+        <li>Carol</li>
+        <li>Wynaut</li>
+        <li>Dia</li>
+        <li>Remote_Romance</li>
+        <li>Freaklunpa</li>
+        <li>a.v.singh</li>
+        <li>dragonfuirt</li>
+        <li>kizen</li>
+        <li>gwalyo</li>
+        <li>roro2676</li>
+        <li>zero9814ld</li>
+        <li>elevampire</li>
+        <li>marvelous seki</li>
+        <li>y tho</li>
+        <li>21stcenturyvelvet</li>
+        <li>squibb0</li>
+        <li>himeyum11</li>
+        <li>gooseman</li>
+        <li>ro boat</li>
+        <li>tos</li>
+        <li>nitchtuna</li>
+        <li>captainblood</li>
+        <li>the_original_insomniac</li>
+        <li>wyvern</li>
+        <li>lenonthedoor</li>
+        <li>yukosuz</li>
+        <li>someonetooksorrow</li>
+        <li>aumiiku</li>
+        <li>giorniogiovanna</li>
+        <li>vimbo</li>
+        <li>cakeh</li>
+        <li>gale_sparkleglow</li>
+        <li>grandadmiralchee</li>
     </ul>
     <p>
         <Link color="red" to="/page2">Go Home</Link>
