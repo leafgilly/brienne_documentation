f52ae0ff1be64a9887cf0fb28741c55750d48bf0
leafgilly <themicrowhale@gmail.com>
2024-06-01 21:40:27 -0400

    added pagesVisited functionality to all girls + css styling

--- my-app/src/pages/DocList.js
+++ my-app/src/pages/DocList.js
@@ -5,29 +5,34 @@ const DocList = () => {
   const pagesVisited = JSON.parse(document.cookie);
   const enableBrienne = pagesVisited["Amanda"] && pagesVisited["Sally"] && pagesVisited["Morgan"] && pagesVisited["Rita"];
   console.log(enableBrienne);
- 
+
+  const disableAmanda = !(pagesVisited["Amanda"]);
+  const disableSally = !(pagesVisited["Sally"]);
+  const disableMorgan = !(pagesVisited["Morgan"]);
+  const disableRita = !(pagesVisited["Rita"]);
+
   return (
     <>
     <p>A permanent link tree for a list of documents.</p>
     <ul>
       <li>
-      <Link to="/Amanda">Amanda</Link>
+      <Link to={disableAmanda ? "/Amanda" : "#"} className={disableAmanda ? 'enabled-link-a' : 'disabled-link'}>Amanda</Link>
       </li>
       <li>
-      <Link to="/Sally">Sally</Link>
+      <Link to={disableSally ? "/Sally" : "#"} className={disableSally ? 'enabled-link-s' : 'disabled-link'}>Sally</Link>
+      {/* <Link to="/Sally">Sally</Link> */}
       </li>
       <li>
-      <Link to="/Morgan">Morgan</Link>
+      <Link to={disableMorgan ? "/Morgan" : "#"} className={disableMorgan ? 'enabled-link-m' : 'disabled-link'}>Morgan</Link>
       </li>
       <li>
-      <Link to="/Rita">Rita</Link>
+      <Link to={disableRita ? "/Rita" : "#"} className={disableRita ? 'enabled-link-r' : 'disabled-link'}>Rita</Link>
       </li>
       {/* <li>
       <Link to="/'Brienne'">"Brienne"</Link>
       </li> */}
       <li>
-      <Link to={enableBrienne ? "/'Brienne'" : "#"} className='disabled-link'>"Brienne"</Link>
-      {/* TODO: UPDATE THE CLASSNAME BASED ON WHETHER OR NOT ENABLEBRIENNE IS TRUE */}
+      <Link to={enableBrienne ? "/'Brienne'" : "#"} className={enableBrienne ? 'enabled-link-b' : 'disabled-link'}>"Brienne"</Link>
       </li>
       <li>
         <Link to="/misc">Misc</Link>
@@ -36,8 +41,6 @@ const DocList = () => {
         <Link to="/">Home</Link>
       </li>
     </ul>
-    {/* <p> ------------- </p>
-     <Amanda /> */}
       <Outlet />
     </>
   )
