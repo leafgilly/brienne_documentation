fb471be50409b656d53a22d3792ce2a3530bb0aa
leafgilly <themicrowhale@gmail.com>
2024-07-16 15:48:26 -0400

    a push of current sally progress in solidarity with piano

--- my-app/src/pages/Sally.js
+++ my-app/src/pages/Sally.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
-import React, {useState, useCallback, useEffect } from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
-import Sound from 'react-sound';
 
 import music from '../audio/SweetDreams.mp3'
 
@@ -137,6 +136,7 @@ const Sally = (props) => {
         <h2 style={{display: eurythmics==='Funkiest' ? 'inline' : 'none'}}><b class='enabled-link-b'>The World's <span style={{color: 'purple'}}>Funkiest</span> Stand</b></h2>
         <h2 style={{display: eurythmics==='Smartest' ? 'inline' : 'none'}}><b class='enabled-link-b'>The World's <span style={{color: 'pink'}}>Smartest</span> Stand</b></h2>
         <h2 style={{display: eurythmics==='Coolest' ? 'inline' : 'none'}}><b class='enabled-link-b'>The World's <span style={{color: 'yellow'}}>Coolest</span> Stand</b></h2>
+        <h2 style={{display: eurythmics==='' ? 'inline' : 'none'}}><b class='enabled-link-b'>The World's ??? Stand</b></h2>
     </div>
 
     <h2 style={{display: props.SallyCorruption<6 ? 'block' : 'none'}}>Eurythmics</h2>
@@ -144,7 +144,7 @@ const Sally = (props) => {
         corrupt(1);
         setEurythmics('Greatest');
     }} class='interactive enabled-link-s no-select-text'>Eurythmics</span></h2>
-    <h2 style={{display: props.SallyCorruption===6 && play && counter!=0 ? 'block' : 'none'}}>Eurythmics</h2>
+    <h2 style={{display: props.SallyCorruption===6 && play && counter!==0 ? 'block' : 'none'}}>Eurythmics</h2>
 
     <p style={{display: props.SallyCorruption<6 ? 'block' : 'none'}}>Eurythmics is a fully sentient robotic Stand that can play music from the speakers in its chest and mouth. It can access public 
         radio waves as well as play music it knows. If anyone moves to the beat of the song Eurythmics plays for four beats, Eurythmics 
@@ -182,32 +182,163 @@ const Sally = (props) => {
         take control of their body. This ability is single target and can be broken if the user is harmed/distracted. 
     </p>
 
-    <p style={{display: props.SallyCorruption>=7 ? 'block' : 'none'}}>[NEW CONTENT HERE]Eurythmics is a fully sentient robotic Stand that can play music from the speakers in its chest and mouth. It can access public
-        radio waves as well as play music it knows. If anyone moves to the beat of the song Eurythmics plays for four beats, Eurythmics 
-        can take control of their body. This ability is single target and can be broken if the user is harmed/distracted. 
+    <h2 style={{display: props.SallyCorruption>=7 ? 'block' : 'none'}}>Eurythmics</h2>
+    <p style={{display: props.SallyCorruption>=7 ? 'block' : 'none'}}>Eurythmics <b class="enabled-link-b">was</b> a fully sentient robotic 
+        Stand that <b class="enabled-link-b">could</b> play music from the speakers in its chest and mouth. It <b class="enabled-link-b">could</b> access public 
+        radio waves as well as play music it <b class="enabled-link-b">knew</b>. If anyone <b class="enabled-link-b">moved</b> to the beat of the song Eurythmics <b class="enabled-link-b">played</b> for four beats,  
+        Eurythmics <b class="enabled-link-b">could</b> take control of their body. This ability <b class="enabled-link-b">was</b> single target and <b class="enabled-link-b">could</b> be broken if the user <b class="enabled-link-b">was</b> harmed/distracted. 
     </p>
 
-    <ul>
+    <ul style={{display: props.SallyCorruption<7 ? 'block' : 'none'}}>
         <li>Note: The single target nature of this Stand is absolute except in unique instances of hivemind crowds.</li>
     </ul>
-    <p>Eurythmics has a strong personality, manifesting in unique mannerisms and preferences for varying its visual appearance. This Stand 
+
+    <ul style={{display: props.SallyCorruption===7 && !play ? 'block' : 'none'}}>
+        <li>Note: The single target nature of this Stand is absolute except in unique instances of hivemind crowds.</li>
+    </ul>
+
+    <ul>
+    <li style={{display: props.SallyCorruption===7 && play && counter===0 ? 'list-item' : 'none'}}>Note: The single target nature of this Stand is absolute <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>except</span> in unique instances of hivemind crowds.</li>
+    <li style={{display: props.SallyCorruption===7 && play && counter===1 ? 'list-item' : 'none'}}>Note: The single target nature of this Stand is absolute except <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>in</span> unique instances of hivemind crowds.</li>
+    <li style={{display: props.SallyCorruption===7 && play && counter===2 ? 'list-item' : 'none'}}>Note: The single target nature of this Stand is absolute except in <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>unique</span> instances of hivemind crowds.</li>
+    <li style={{display: props.SallyCorruption===7 && play && counter===3 ? 'list-item' : 'none'}}>Note: The single target nature of this Stand is absolute except in unique <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>instances</span> of hivemind crowds.</li>
+    </ul>
+    <ul style={{display: props.SallyCorruption>=8 ? 'block' : 'none'}}>
+        <li><b class="enabled-link-b">It tried so hard to save her. It buried its fingers in her flesh, trying to pull the arrow out.</b> Except in unique instances, <b class="enabled-link-b">it always knew how to protect her.</b></li>
+    </ul>
+
+    <p style={{display: props.SallyCorruption<8 ? 'block' : 'none'}}>!Eurythmics has a strong personality, manifesting in unique mannerisms and preferences for varying its visual appearance. This Stand 
         has been observed having the ability to change its physical manifestation at will (i.e. Michael Jackson’s Thriller jumpsuit, 
         two feather boas instead of one).</p>
+    <p style={{display: props.SallyCorruption===8 && play && counter===0 ? 'block' : 'none'}}>Eurythmics has a strong personality, manifesting in unique mannerisms and preferences for varying its visual appearance. This Stand 
+        has been observed having the ability to change its physical manifestation at will (i.e. Michael Jackson’s Thriller jumpsuit,&nbsp;
+            <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>two</span> feather boas instead of one).</p>
+    <p style={{display: props.SallyCorruption===8 && play && counter===1 ? 'block' : 'none'}}>Eurythmics has a strong personality, manifesting in unique mannerisms and preferences for varying its visual appearance. This Stand 
+        has been observed having the ability to change its physical manifestation at will (i.e. Michael Jackson’s Thriller jumpsuit, 
+        two feather boas <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>instead</span> of one).</p>
+    <p style={{display: props.SallyCorruption===8 && play && counter===2 ? 'block' : 'none'}}>Eurythmics has a strong personality, manifesting in unique mannerisms and preferences for varying its visual appearance. This Stand 
+        has been observed having the ability to change its physical manifestation at will (i.e. Michael Jackson’s Thriller jumpsuit, 
+        two feather boas instead <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>of</span> one).</p>
+    <p style={{display: props.SallyCorruption===8 && play && counter===3 ? 'block' : 'none'}}>Eurythmics has a strong personality, manifesting in unique mannerisms and preferences for varying its visual appearance. This Stand 
+        has been observed having the ability to change its physical manifestation at will (i.e. Michael Jackson’s Thriller jumpsuit, 
+        two feather boas instead of <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>one</span>).</p>
+    <p style={{display: props.SallyCorruption===8 && !play ? 'block' : 'none'}}>Eurythmics has a strong personality, manifesting in unique mannerisms and preferences for varying its visual appearance. This Stand 
+        has been observed having the ability to change its physical manifestation at will (i.e. Michael Jackson’s Thriller jumpsuit, 
+        two feather boas instead of one).</p>
+    <p style={{display: props.SallyCorruption>=9 ? 'block' : 'none'}}>Eurythmics has a strong personality <b class='enabled-link-b'>just like mine</b>, manifesting in <b class='enabled-link-b'>queer behavior</b> and preferences for <b class='enabled-link-b'>affecting my</b> visual appearance. This Stand 
+        has been observed having the ability to <b class='enabled-link-b'>rewrite a life at will</b> (i.e. <b class='enabled-link-b'>I'm sorry I'm sorry I'M SORRY</b>).</p>
+
+
     <h2>Known Relationships</h2>
-    <ul>
+    <ul style={{display: props.SallyCorruption<9 ? 'block' : 'none'}}>
         <li>Norman Reed: father. Amicable but distant relationship. He is a man of few words, she struggled to broach that.
         </li>
-        <li>Lydia Reed: mother. Strained relationship. Wishes she cared more about Sally’s academics than grandchildren.
+        <li>Lydia Reed: mother. Strained relationship. Wishes she worried over Sally’s academics more than grandchildren.
         </li>
-        <li>{props.AmandaName}: Coworker, close friend. Relied on her as an authority figure after learning about Stands; 
+        <li>{props.AmandaName}: Coworker, close friend. Relied on her as an authority figure to learn about Stands; 
             often had her stay overnight for peace of mind soon after Sally’s assault with the arrow.
         </li>
-        <li>{props.MorganName}: Coworker, close friend. Occasionally intimidated by her wealth. Deeply values her carefree, positive nature.
+        <li>{props.MorganName}: Coworker, close friend. Occasionally intimidated by her wealth. Grateful for her carefree, positive nature.
         </li>
         <li>{props.RitaName}: Coworker, close friend. Always go out for coffee and lunch hours together during slow days. Loves teasing her, 
             but appreciates her for being her most normal friend at work.
         </li>
     </ul>
+
+    <ul style={{display: props.SallyCorruption===9 && play ? 'block' : 'none'}}>
+        <li style={{display: counter===0 ? 'list-item' : 'none'}}>Norman Reed: father. <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>A</span>micable but distant relationship. He is a man of few words, she struggled to broach that.
+        </li>
+        <li style={{display: counter!==0 ? 'list-item' : 'none'}}>Norman Reed: father. Amicable but distant relationship. He is a man of few words, she struggled to broach that.
+        </li>
+        <li style={{display: counter===1 ? 'list-item' : 'none'}}>Lydia Reed: mother. Strained relationship. Wishes she worried <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>over</span> Sally’s academics more than grandchildren.
+        </li>
+        <li style={{display: counter!==1 ? 'list-item' : 'none'}}>Lydia Reed: mother. Strained relationship. Wishes she worried over Sally’s academics more than grandchildren.
+        </li>
+        <li style={{display: counter===2 ? 'list-item' : 'none'}}>{props.AmandaName}: Coworker, close friend. Relied on her as an authority figure <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>to</span> learn about Stands; 
+            often had her stay overnight for peace of mind soon after Sally’s assault with the arrow.
+        </li>
+        <li style={{display: counter!==2 ? 'list-item' : 'none'}}>{props.AmandaName}: Coworker, close friend. Relied on her as an authority figure to learn about Stands; 
+            often had her stay overnight for peace of mind soon after Sally’s assault with the arrow.
+        </li>
+        <li style={{display: counter===3 ? 'list-item' : 'none'}}>{props.MorganName}: Coworker, close friend. Occasionally intimidated by her wealth. <span onClick={()=>{
+        corrupt(1);
+    }} class='interactive enabled-link-s no-select-text'>G</span>rateful for her carefree, positive nature.
+        </li>
+        <li style={{display: counter!==3 ? 'list-item' : 'none'}}>{props.MorganName}: Coworker, close friend. Occasionally intimidated by her wealth. Grateful for her carefree, positive nature.
+        </li>
+        <li>{props.RitaName}: Coworker, close friend. Always go out for coffee and lunch hours together during slow days. Loves teasing her, 
+            but appreciates her for being her most normal friend at work.
+        </li>
+    </ul>
+
+    <ul style={{display: props.SallyCorruption===9 && !play ? 'block' : 'none'}}>
+        <li>Norman Reed: father. Amicable but distant relationship. He is a man of few words, she struggled to broach that.
+        </li>
+        <li>Lydia Reed: mother. Strained relationship. Wishes she worried over Sally’s academics more than grandchildren.
+        </li>
+        <li>{props.AmandaName}: Coworker, close friend. Relied on her as an authority figure to learn about Stands; 
+            often had her stay overnight for peace of mind soon after Sally’s assault with the arrow.
+        </li>
+        <li>{props.MorganName}: Coworker, close friend. Occasionally intimidated by her wealth. Grateful for her carefree, positive nature.
+        </li>
+        <li>{props.RitaName}: Coworker, close friend. Always go out for coffee and lunch hours together during slow days. Loves teasing her, 
+            but appreciates her for being her most normal friend at work.
+        </li>
+    </ul>
+    <ul style={{display: props.SallyCorruption>=10 ? 'block' : 'none'}}>
+        <li>Norman Reed: <b class='enabled-link-b'>fa&nbsp;&nbsp;e&nbsp;.&nbsp;A&nbsp;&nbsp;cab&nbsp;e&nbsp;b&nbsp;&nbsp;&nbsp;d
+            &nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;
+            a&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d&nbsp;,&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;&nbsp;
+            gg&nbsp;ed&nbsp;&nbsp;&nbsp;&nbsp;b&nbsp;&nbsp;ac&nbsp;&nbsp;&nbsp;&nbsp;a&nbsp;.</b>
+        </li>
+        <li>Lydia Reed: <b class='enabled-link-b'>&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;a&nbsp;&nbsp;ed&nbsp;&nbsp;e&nbsp;a
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            ed&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;’&nbsp;&nbsp;acade&nbsp;&nbsp;c&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;a
+            &nbsp;g&nbsp;a&nbsp;dc&nbsp;&nbsp;&nbsp;d&nbsp;e&nbsp;.</b>
+        </li>
+        <li>{props.AmandaName}: <b class='enabled-link-b'>C&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;,&nbsp;c&nbsp;&nbsp;&nbsp;e&nbsp;f&nbsp;&nbsp;e&nbsp;d.
+            &nbsp;e&nbsp;&nbsp;ed&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;a&nbsp;&nbsp;a&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            f&nbsp;g&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ea&nbsp;&nbsp;&nbsp;ab&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a&nbsp;d&nbsp;; 
+        &nbsp;f&nbsp;e&nbsp;&nbsp;&nbsp;ad&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;g&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;eace&nbsp;&nbsp;f&nbsp;&nbsp;&nbsp;&nbsp;d&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;af&nbsp;e&nbsp;&nbsp;&nbsp;a&nbsp;&nbsp;
+        ’&nbsp;&nbsp;a&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;.</b>
+        </li>
+        <li>{props.MorganName}: <b class='enabled-link-b'>C&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;,&nbsp;c&nbsp;&nbsp;&nbsp;e&nbsp;f&nbsp;
+            e&nbsp;d.&nbsp;&nbsp;cca&nbsp;&nbsp;&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;da&nbsp;ed&nbsp;b
+            &nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;ea&nbsp;&nbsp;&nbsp;.&nbsp;G&nbsp;a&nbsp;ef&nbsp;&nbsp;&nbsp;f&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;ca
+            ef&nbsp;ee,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;e.</b>
+        </li>
+        <li>{props.RitaName}: <b class='enabled-link-b'>C&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;,&nbsp;c&nbsp;&nbsp;&nbsp;e&nbsp;f&nbsp;&nbsp;e
+            d.&nbsp;A&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;g&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;f&nbsp;&nbsp;&nbsp;c&nbsp;ffee&nbsp;a&nbsp;d&nbsp;&nbsp;&nbsp;
+            c&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ge&nbsp;&nbsp;e&nbsp;&nbsp;d&nbsp;&nbsp;&nbsp;&nbsp;g&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            da&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;ea&nbsp;&nbsp;&nbsp;g&nbsp;&nbsp;e&nbsp;, 
+            b&nbsp;&nbsp;&nbsp;a&nbsp;&nbsp;&nbsp;ec&nbsp;a&nbsp;e&nbsp;&nbsp;&nbsp;e&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;be&nbsp;&nbsp;g&nbsp;
+            e&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a&nbsp;&nbsp;f&nbsp;&nbsp;&nbsp;&nbsp;d&nbsp;a&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.</b>
+        </li>
+    </ul>
+
     <h2>Psychological Profile</h2>
     <p>Reed is highly intelligent and perceptive with great aptitude for leadership, although she struggles with being unable to perfectly 
         control people. The latter quality has developed into a form of survivor’s guilt: she hates watching people get injured to save 
