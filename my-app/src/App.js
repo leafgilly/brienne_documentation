897c650cb565ea23f293443b66902e85d870e2fd
leafgilly <themicrowhale@gmail.com>
2024-07-23 11:06:27 -0400

    first functioning buikd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 // import { Form, Field } from 'react-final-form'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route, HashRouter } from "react-router-dom";
 import Amanda from "./pages/Amanda";
 import Sally from "./pages/Sally";
 import Sally2 from "./pages/Sally2";
@@ -17,22 +17,22 @@ function App() {
 
   return (
     <Styles>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/">
         <Route index element={<Page2 />} />
         {/* <Route index element={<Page1 />} /> */}
-        <Route path="Amanda" element={<Amanda />} />
-        <Route path="Sally" element={<Sally />} />
-        <Route path="Morgan" element={<Morgan />} />
-        <Route path="Rita" element={<Rita />} />
-        <Route path="theAgent" element={<Brienne />} />
-        <Route path="misc" element={<Misc />} />
-        <Route path="Sally/prank" element={<Sally2 />} />
-        <Route path="Brienne/goodend" element={<Brienne2 />} />
+        <Route path="/Amanda" element={<Amanda />} />
+        <Route path="/Sally" element={<Sally />} />
+        <Route path="/Morgan" element={<Morgan />} />
+        <Route path="/Rita" element={<Rita />} />
+        <Route path="/theAgent" element={<Brienne />} />
+        <Route path="/misc" element={<Misc />} />
+        <Route path="/Sally/prank" element={<Sally2 />} />
+        <Route path="/Brienne/goodend" element={<Brienne2 />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </Styles>
   );
 }
